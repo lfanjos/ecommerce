0c83c944dc5d68391aad7fd1221ffe07ddf1c60d
Lucas <lfanjosus@gmail.com>
2020-11-06 13:23:50 -0300

    criei a classe Page

--- index.php
+++ index.php
@@ -2,17 +2,19 @@
 
 require_once("vendor/autoload.php");
 
-$app = new \Slim\Slim();
+use \Slim\Slim;
+use \lfanjos\page;
+
+$app = new Slim();
 
 $app->config('debug', true);
 
 $app->get('/', function() {
 
-	$sql = new lfanjos\DB\Sql();
+	$page = new Page();
 
-	$results = $sql->select("SELECT * FROM tb_users");
-
-	echo json_encode($results);
+	$page->setTpl("index");
+	
 
 });
 
